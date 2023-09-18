686b8713dbb964360517b33d1307e26ded2fe191
MacBook <macbook@192.168.1.115>
2023-09-18 17:26:04 +0300

    sevenLeson

---
+++ Homework/FirstLeson/sevenLeson.js
@@ -0,0 +1,26 @@
+"use srtict";
+
+// 1 задача 
+
+// function addThemAll(...args) {
+//     return args.reduce((sum, current) => sum + current, 0);
+//   }
+  
+//   console.log(addThemAll(2, 4)); 
+//   console.log(addThemAll(1, 2, 3, 4)); 
+//   console.log(addThemAll(5, 5, 10)); 
+
+
+// 2 задача 
+
+// function multiply(a) {
+//     return function(b) {
+//       return a * b;
+//     };
+//   }
+  
+//   console.log(multiply(5)(5));  
+//   console.log(multiply(2)(-2)); 
+//   console.log(multiply(4)(3));  
+  
+  
