072a6313f3d021f51489a6c289b982d7cac24c15
MacBook <macbook@192.168.1.106>
2023-10-02 18:59:53 +0300

    twelveLeson

--- Homework/FirstLeson/twelveLeson.js
+++ Homework/FirstLeson/twelveLeson.js
@@ -2,29 +2,51 @@
 
 let colorChaing = document.querySelector(".redBtn");
 console.log(colorChaing);
+const body = document.body;
 
-colorChaing.addEventListener("click", () => {
-    if (document.body.classList.contains("lightMod")) {
-        document.body.classList.remove("lightMod");
-        document.body.classList.add("darkMod");
-        colorChaing.textContent = "Turn on";
+function setMode(state) {
+    if (state === "off") {
+        body.classList.remove("lightMode");
+        body.classList.add("darkMode");
+        colorChaing.textContent = "Turn ON";
         const newElement = document.createElement("p");
-        const timestamp = Date.now();
-        const currentDate = new Date(timestamp);
-        const options = { day: "numeric", month: "long", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
-        newElement.textContent = "Last turn off : " + currentDate.toLocaleString(options);
-        document.body.appendChild(newElement);
-        localStorage.setItem("day", JSON.stringify(currentDate));
+        const formattedDate = formatCurrentDate();
+        newElement.textContent = "Last turn OFF : " + formattedDate;
+        body.appendChild(newElement);
     } else {
-        document.body.classList.remove("darkMod");
-        document.body.classList.add("lightMod");
+        body.classList.remove("darkMode");
+        body.classList.add("lightMode");
         colorChaing.textContent = "Turn OFF";
         const newElement = document.createElement("p");
-        const timestamp = Date.now();
-        const currentDate = new Date(timestamp);
-        const options = { day: "numeric", month: "long", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
-        newElement.textContent = "Last turn ON : " + currentDate.toLocaleString(options);
-        document.body.appendChild(newElement);
-        localStorage.setItem("night", JSON.stringify(currentDate));
+        const formattedDate = formatCurrentDate();
+        newElement.textContent = "Last turn ON : " + formattedDate;
+        body.appendChild(newElement);
     }
+    localStorage.setItem("colorMode", state);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const currentState = localStorage.getItem("colorMode") || "off";
+    setMode(currentState);
 });
+
+colorChaing.addEventListener("click", () => {
+    const currentState = localStorage.getItem("colorMode") || "off";
+    const newState = currentState === "off" ? "on" : "off";
+    setMode(newState);
+});
+
+function formatCurrentDate() {
+    const now = new Date();
+
+    const day = now.getDate().toString().padStart(2, "0");
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const year = now.getFullYear().toString();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    const formattedDate = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+
+    return formattedDate;
+}
